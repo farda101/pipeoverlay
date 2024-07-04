9ff8c1ec9e90c1555372c7ff56978f5053329436
farda101 <21090101.farda@student.poltektegal.ac.id>
2024-07-04 16:19:41 +0700

    tryon

--- main.py
+++ main.py
@@ -1,35 +1,69 @@
+import os
+
 import cvzone
 import cv2
 from cvzone.PoseModule import PoseDetector
-import mediapipe as mp
-import os
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("Resources/Videos/1.mp4")
 detector = PoseDetector()
 
-shirtPath = "Resources/Shirts"
-listShirts = os.listdir(shirtPath)
-print(listShirts)
+shirtFolderPath = "Resources/Shirts"
+listShirts = os.listdir(shirtFolderPath)
+# print(listShirts)
+fixedRatio = 262 / 190  # widthOfShirt/widthOfPoint11to12
+shirtRatioHeightWidth = 581 / 440
+imageNumber = 0
+imgButtonRight = cv2.imread("Resources/button.png", cv2.IMREAD_UNCHANGED)
+imgButtonLeft = cv2.flip(imgButtonRight, 1)
+counterRight = 0
+counterLeft = 0
+selectionSpeed = 10
 
 while True:
     success, img = cap.read()
     img = detector.findPose(img)
+    # img = cv2.flip(img,1)
     lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False, draw=False)
     if lmList:
-        #center = bboxInfo["center"]
-        #cv2.circle(img, center, 5, (255, 0, 255), cv2.FILLED)
-        
+        # center = bboxInfo["center"]
         lm11 = lmList[11][1:3]
         lm12 = lmList[12][1:3]
-        lm13 = lmList[13][1:3]
-        imgShirt = cv2.imread(os.path.join(shirtPath, listShirts[0]), cv2.IMREAD_UNCHANGED)
-        #img = cvzone.overlayPNG(img, imgShirt,(100,100))
-        imgShirt = cv2.resize(imgShirt,(0,0),None,0.5,0.5)
+        imgShirt = cv2.imread(os.path.join(shirtFolderPath, listShirts[imageNumber]), cv2.IMREAD_UNCHANGED)
+
+        widthOfShirt = int((lm11[0] - lm12[0]) * fixedRatio)
+        print(widthOfShirt)
+        imgShirt = cv2.resize(imgShirt, (widthOfShirt, int(widthOfShirt * shirtRatioHeightWidth)))
+        currentScale = (lm11[0] - lm12[0]) / 190
+        offset = int(44 * currentScale), int(48 * currentScale)
+
         try:
-            img = cvzone.overlayPNG(img, imgShirt, lm13)
+            img = cvzone.overlayPNG(img, imgShirt, (lm12[0] - offset[0], lm12[1] - offset[1]))
         except:
             pass
-     
-     
+
+        img = cvzone.overlayPNG(img, imgButtonRight, (1074, 293))
+        img = cvzone.overlayPNG(img, imgButtonLeft, (72, 293))
+
+        if lmList[16][1] < 300:
+            counterRight += 1
+            cv2.ellipse(img, (139, 360), (66, 66), 0, 0,
+                        counterRight * selectionSpeed, (0, 255, 0), 20)
+            if counterRight * selectionSpeed > 360:
+                counterRight = 0
+                if imageNumber < len(listShirts) - 1:
+                    imageNumber += 1
+        elif lmList[15][1] > 900:
+            counterLeft += 1
+            cv2.ellipse(img, (1138, 360), (66, 66), 0, 0,
+                        counterLeft * selectionSpeed, (0, 255, 0), 20)
+            if counterLeft * selectionSpeed > 360:
+                counterLeft = 0
+                if imageNumber > 0:
+                    imageNumber -= 1
+
+        else:
+            counterRight = 0
+            counterLeft = 0
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
